bbf0becfe0eda77a5cab8025546554e5f0fca71a
Cole Diffin <2082764+arcticShadow@users.noreply.github.com>
2024-11-13 23:12:39 +1300

    Android asdf plugin init commit

--- README.md
+++ README.md
@@ -17,34 +17,73 @@
 
 **TODO: adapt this section**
 
-- `bash`, `curl`, `tar`, and [POSIX utilities](https://pubs.opengroup.org/onlinepubs/9699919799/idx/utilities.html).
-- `SOME_ENV_VAR`: set this environment variable in your shell config to load the correct version of tool x.
+- `bash`, `curl`, `unzip`, and [POSIX utilities](https://pubs.opengroup.org/onlinepubs/9699919799/idx/utilities.html).
 
 # Install
 
 Plugin:
 
 ```shell
-asdf plugin add android
-# or
+#asdf
 asdf plugin add android https://github.com/arcticShadow/asdf-android.git
+
+# or, in mise
+mise plugins install https://github.com/arcticShadow/asdf-android.git
 ```
 
 android:
 
 ```shell
-# Show all installable versions
+#
+# ASDF
+#
+
+# Show all installable versions - becasue of the way this plugin works, there will likly only be one 
 asdf list-all android
 
 # Install specific version
 asdf install android latest
 
-# Set a version globally (on your ~/.tool-versions file)
-asdf global android latest
+# Now android commands are available
+sdkmanager --version
+
+#
+# mise
+#
+
+# save the tool version to the mise config
+mise use android
 
 # Now android commands are available
 sdkmanager --version
 ```
+
+# How it works. 
+
+This plugin will attempt to locate the Android SDK in a number of common locations. if _not_ found, it will download a temporary copy of cmdline-tools, and use it to setup a root sdk location.
+
+If the Android SDK can be located on your system, this plugin will use it, and references to android tools will point to the respective sdk location.
+
+# Install Android packages (mise only)
+
+In mise, we can take this step further with tool options. You can specify which Android SDK packages to install in your `.mise.toml` configuration file:
+
+```toml
+[tools]
+android = { build-tools = "35.0.0", platforms = "android-31" }
+```
+
+
+After adding packages to your tool options, run `mise install` to install the specified Android SDK packages.
+
+You can also install packages manually using the `sdkmanager` command:
+
+```shell
+sdkmanager "platforms;android-34" "build-tools;34.0.0"
+```
+
+# other android tools
+if you install the platform-tools, or build-tools - then the plugin will expose these tools in your path for you.
 
 Check [asdf](https://github.com/asdf-vm/asdf) readme for more instructions on how to
 install & manage versions.
--- lib/utils.bash
+++ lib/utils.bash
@@ -2,10 +2,10 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for android.
-GH_REPO="https://github.com/arcticShadow/asdf-android"
+
+ANDROID_COMMAND_LINE_TOOLS_PATH="cmdline-tools"
 TOOL_NAME="android"
-TOOL_TEST="sdkmanager --version"
+TOOL_TEST="${ANDROID_COMMAND_LINE_TOOLS_PATH}/bin/sdkmanager --version"
 
 fail() {
 	echo -e "asdf-$TOOL_NAME: $*"
@@ -14,61 +14,172 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if android is not hosted on GitHub releases.
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
 
 sort_versions() {
 	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
 		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
-list_github_tags() {
-	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
-}
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if android has other means of determining installable versions.
-	list_github_tags
+	# this is the version of the 'asdf-android' tool itself. technically - it installs androids sdk manager from cmdlinetools but we can actually use that, to update itself...
+	# mabey revert this to be the github tags in future?
+	local versions=(1)
+
+	echo "${versions[@]}"
 }
 
 download_release() {
-	local version filename url
+	local version filename url platform android_sdk_root
 	version="$1"
 	filename="$2"
+	
+	platform=$(get_platform)
+	android_sdk_root=$(find_android_sdk_root)
 
-	# TODO: Adapt the release URL convention for android
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	# Check if file already exists
+	if [ -f "$filename" ]; then
+		echo "* File $filename already exists, skipping download..."
+		return 0
+	fi
+
+	# If sdkmanager already exists in SDK root, skip download
+	if [ -n "$android_sdk_root" ] && [ -x "$android_sdk_root/$ANDROID_COMMAND_LINE_TOOLS_PATH/latest/bin/sdkmanager" ]; then
+		echo "* Found existing sdkmanager in $android_sdk_root, skipping download..."
+		return 0
+	fi
+
+	url="https://dl.google.com/android/repository/commandlinetools-${platform}-${version}_latest.zip"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
 }
 
-install_version() {
+install_command_line_tools() {
 	local install_type="$1"
 	local version="$2"
-	local install_path="${3%/bin}/bin"
+
+	local android_sdk_root=$(find_android_sdk_root)
 
 	if [ "$install_type" != "version" ]; then
 		fail "asdf-$TOOL_NAME supports release installs only"
 	fi
 
 	(
-		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert android executable exists.
-		local tool_cmd
-		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+		# First check if sdkmanager exists in existing SDK installation
+		local sdkmanager=""
+		local cmdlinetools_version=""
+		if [ -n "$android_sdk_root" ] && [ -d "$android_sdk_root/$ANDROID_COMMAND_LINE_TOOLS_PATH/latest" ]; then
+			if [ -x "$android_sdk_root/$ANDROID_COMMAND_LINE_TOOLS_PATH/latest/bin/sdkmanager" ]; then
+				sdkmanager="$android_sdk_root/$ANDROID_COMMAND_LINE_TOOLS_PATH/latest/bin/sdkmanager"
+			fi
+		fi
+
+		#Otherwise, use sdkmanager from extracted download (download should have occured if it was unabel to locate an sdkmanager)
+		if [ -z "$sdkmanager" ]; then
+			sdkmanager="${ASDF_DOWNLOAD_PATH}/extracted/${ANDROID_COMMAND_LINE_TOOLS_PATH}/bin/sdkmanager"
+		fi
+		
+		# check version of cmdline-tools
+		cmdlinetools_version=$($sdkmanager --list --sdk_root=$android_sdk_root | grep "cmdline-tools;latest" | awk -F'|' '{print $2}' | tr -d ' ' | head -n1)
+		
+		# install cmdline-tools
+		install_package "cmdline-tools;latest"
+
+		# get installed version of cmdline-tools
+		local installed_version
+		installed_version=$($sdkmanager --list_installed --sdk_root=$android_sdk_root| grep "cmdline-tools;latest" | awk -F'|' '{print $2}' | tr -d ' ')
+		
+		# Verify cmdline-tools version matches expected version
+		if [ "$installed_version" != "$cmdlinetools_version" ]; then
+			echo "* Installed cmdline-tools version ($installed_version) does not match expected version ($cmdlinetools_version)"
+			fail "Expected versions to match"
+		fi
+		
 
 		echo "$TOOL_NAME $version installation was successful!"
 	) || (
-		rm -rf "$install_path"
 		fail "An error occurred while installing $TOOL_NAME $version."
 	)
+
+}
+
+find_android_sdk_root() {
+    # Check common Android SDK locations
+    local sdk_locations=(
+        "$HOME/Library/Android/sdk"           # macOS default
+        "$HOME/Android/Sdk"                   # Linux default
+        "${LOCALAPPDATA:-}/Android/sdk"           # Windows default
+        "${ANDROID_HOME:-}"                       # Legacy env var
+        "${ANDROID_SDK_ROOT:-}"                   # Current env var
+    )
+
+    for location in "${sdk_locations[@]}"; do
+        if [ -d "$location" ]; then
+            echo "$location"
+            return 0
+        fi
+    done
+
+    # Fall back to ASDF install path if no existing SDK found
+    echo "${ASDF_INSTALL_PATH%/bin}"
+    return 0
+}
+
+install_package() {
+	local package="$1"
+	android_sdk_root=$(find_android_sdk_root)
+	sdkmanager="${android_sdk_root}/${ANDROID_COMMAND_LINE_TOOLS_PATH}/latest/bin/sdkmanager"
+
+	echo "* Installing Android package: $package"
+	$sdkmanager --install --sdk_root="${android_sdk_root}" $package <<< "y" 
+}
+
+# This function is only useful when using mise - in asdf it is ignored
+install_additional_tools() {
+	local install_path="${1%/bin}/"
+	
+	# Get all MISE_TOOL_OPTS__ environment variables
+	local sdk_packages=()
+	while IFS='=' read -r name value; do
+		if [[ $name == MISE_TOOL_OPTS__* ]]; then
+			# Extract the part after MISE_TOOL_OPTS__
+			local package_name=${name#MISE_TOOL_OPTS__}
+			# Convert to lowercase and replace _ with -
+			package_name=$(echo "$package_name" | tr '[:upper:]' '[:lower:]' | tr '_' '-')
+			# Add package name and version to array
+			if [ "$value" = "latest" ]; then
+				sdk_packages+=("$package_name")
+			else
+				sdk_packages+=("$package_name;$value")
+			fi
+		fi
+	done < <(env)
+
+	# Install each package using sdkmanager
+	for package in "${sdk_packages[@]}"; do
+		install_package "$package"
+	done
+}
+
+get_platform() {
+	local platform;
+
+	case "$(uname -s)" in
+		Darwin*)
+			platform="mac"
+			;;
+		Linux*)
+			platform="linux" 
+			;;
+		MINGW*|MSYS*|CYGWIN*)
+			platform="win"
+			;;
+		*)
+			fail "Unsupported platform: $(uname -s)"
+			;;
+	esac
+
+	echo "$platform"
 }
